70a88587cf3b4e4a42e8079400b193d12ccec4f4
apelsin16 <apelsin@i.ua>
2017-07-31 15:28:59 +0300

    15 ready

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@
 
 
 
-  var btn = document.querySelector('button'),
-      input = document.querySelector('input');
+  var btn = document.getElementById('button'),
+      input = document.getElementById('input');
 
 
 function searchInput(){
   var container = document.getElementById('photo');
-  console.log(container);
+//  console.log(container);
   var source = document.getElementById('template').textContent;
-  console.log(source);
+//  console.log(source);
   var compiled = _.template(source);
-  console.log(compiled);
+//  console.log(compiled);
   var apiURL = createAPI();
-  console.log(apiURL);
+//  console.log(apiURL);
   var promise = fetch(apiURL)
   .then(function(response){
     if (response.ok){
@@ -31,7 +31,7 @@ function searchInput(){
           };
 
     });
-    console.log(imgs);
+    // console.log(imgs);
     render(compiled, imgs, container);
   });
 }
@@ -55,7 +55,7 @@ function createAPI (){
   // var endpoint = 'https://pixabay.com/api/?key=5992000-de0663331a4d0b877fb659d73&q=';
   // var other = '&orientation=horizontal';
   var q = input.value;
-  console.log(q);
+  // console.log(q);
   // var apiURL = endpoint + q + other;
   //https://pixabay.com/api/?key=5992000-de0663331a4d0b877fb659d73&q=yellow+flowers&image_type=photo&pretty=true
   return ('https://pixabay.com/api/?key=5992000-de0663331a4d0b877fb659d73&q=' + q + '&orientation=horizontal');
@@ -63,5 +63,62 @@ function createAPI (){
   btn.addEventListener('click', function(){
     searchInput();
   });
+
+var question = document.getElementById('question'),
+    answers = document.getElementById('answers'),
+    correctAnswer = document.getElementById('correctAnswer'),
+    submit = document.getElementById('submit');
+
+function TestConstructor (question, answers, correctAnswer){
+  this.question = question;
+  this.answers = answers;
+  this.correctAnswer = correctAnswer;
+}
+
+//localStorage.clear();
+var getArray = [];
+localStorage.setItem('storageElement', '[]');
+
+
+  function pushStorage (test){
+      getArray.push(test);
+      localStorage.setItem('storageElement',JSON.stringify(test));
+      getArray= JSON.parse(localStorage.getItem('storageElement'));
+    }
+
+submit.addEventListener('click', function(){
+
+create();
+
+});
+
+
+
+
+function makeTemplate(dataTest) {
+    var containerTest = document.getElementById('containerTest'),
+    sourceTest = document.getElementById('templateTest').textContent,
+    templateTest = _.template(sourceTest);
+    containerTest.innerHTML += templateTest(dataTest);
+  }
+
+function create (){
+  var arrQuestion = question.value,
+      arrAnswers = answers.value,
+      newArrAnswers = arrAnswers.split(','),
+      arrCorrectAnswer = correctAnswer.value;
+
+
+   var test = new TestConstructor(arrQuestion, newArrAnswers, arrCorrectAnswer);
+
+   pushStorage(test);
+   makeTemplate(getArray);
+   getArray = [];
+}
+
+
+
+
+
 
 })();
